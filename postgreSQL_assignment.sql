596ccd78258f38c3f8c49149b20a41932b76bd34
Naimur Rahman <naimur2231@gmail.com>
2024-03-05 01:41:04 +0600

    Calculate and display the average age of all employees

--- postgreSQL_assignment.sql
+++ postgreSQL_assignment.sql
@@ -58,3 +58,7 @@ SELECT * FROM employees WHERE salary > 60000;
 
 --Query 2: Retrieve the names of employees using a limit of 2, starting from the 3rd employee.
 SELECT employee_name FROM employees LIMIT 2 OFFSET 2 * 1;
+
+
+--Query 3: Calculate and display the average age of all employees.
+SELECT ROUND(AVG(age),1) AS average_age FROM employees;
